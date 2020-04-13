d8a97c77233fcfe36fd10629ea12252b846ef316
ben1kk <benjamin.krueger@ymail.com>
2020-04-13 12:15:23 +0200

    Struktur und make.bat für mkdocs Integration vorbereitet

--- de/make.bat
+++ de/make.bat
@@ -19,25 +19,25 @@ echo -- Old files deleted
 
 REM Create Microsoft Word Version (docx)
 echo -- Create Word document
-pandoc -s -o %filename%.docx %filename%.md --metadata-file metadata.yaml
+pandoc metadata.yaml -s --resource-path="./src" ./src/%filename%.md -o %filename%.docx
 echo -- Word document created
 
 REM Create Web Version (html)
 echo -- Create HTML document
-pandoc -s --toc -o %filename%.html %filename%.md --metadata-file metadata.yaml
+pandoc metadata.yaml -s --resource-path="./src" --toc ./src/%filename%.md -o %filename%.html
 echo -- HTML document created
 
 REM Create PDF Version (pdf)
 echo -- Create PDF document
-pandoc %filename%.md metadata.yaml -o %filename%.pdf --from markdown --template sketchnotes --number-sections -V lang=de-de
+pandoc metadata.yaml --from markdown --resource-path="./src" --template sketchnotes --number-sections -V lang=de-de ./src/%filename%.md -o %filename%.pdf 
 echo -- PDF document created
 
 REM Create eBook Versions (epub, mobi)
 echo -- Create eBook dokuments
 echo - Create cover
-magick convert -density 300 %filename%.pdf[0] images/ebook-cover.png
+magick convert -density 300 %filename%.pdf[0] src/images/ebook-cover.png
 echo - Create epub-document
-pandoc -s --epub-cover-image=images/ebook-cover.png -o %filename%.epub %filename%.md --metadata-file metadata.yaml
+pandoc metadata.yaml -s --resource-path="./src" --epub-cover-image=src/images/ebook-cover.png ./src/%filename%.md -o %filename%.epub
 echo - Convert epub-document to epub-document
 ebook-convert %filename%.epub %filename%.mobi
 
